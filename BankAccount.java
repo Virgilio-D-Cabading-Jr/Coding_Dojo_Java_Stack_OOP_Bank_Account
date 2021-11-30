22f57ffa8548370b44c66b88042ae30909a46c89
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-11-29 21:30:20 -0800

    implement BankAccount display

--- BankAccount.java
+++ BankAccount.java
@@ -4,5 +4,37 @@
 //  /////////////////////////////////////////////////////////////
 
 public class BankAccount {
-    
+    //  //// MEMBER VARIABLES ///////////////////////////////////
+    private double checkingBalance;
+    private double savingsBalance;
+    private long accountNumber;
+    private static int numTotalBankAccounts = 0;
+    private static double totalAccountBalance = 0;
+
+    //  //// CONSTRUCTORS ///////////////////////////////////////
+    public BankAccount() {
+        this.checkingBalance = 0;
+        this.savingsBalance = 0;
+        BankAccount.numTotalBankAccounts++;
+        this.accountNumber = 100000000 - (BankAccount.numTotalBankAccounts*7);
+    }
+
+    //  //// SET METHODS ////////////////////////////////////////
+
+    //  //// GET METHODS ////////////////////////////////////////
+
+    public static int getNumTotalBankAccounts() {
+        return BankAccount.numTotalBankAccounts;
+    }
+
+    public static double getTotalBankAccountBalance() {
+        return BankAccount.totalAccountBalance;
+    }
+
+    //  //// SHOW METHODS ///////////////////////////////////////
+
+    public void display() {
+        System.out.printf("---- Bank Account Num: %d ------------\n", this.accountNumber);
+        System.out.printf("Checking Balance: $%.2f || Savings Balance: $%.2f\n", this.checkingBalance, this.savingsBalance);
+    }
 }
--- BankAccountApp.java
+++ BankAccountApp.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;                                         // Import ArrayLists
+
 //  /////////////////////////////////////////////////////////////
 //  BANK ACCOUNT APP
 //  By: Virgilio D. Cabading Jr.    Created at: November 29, 2021 2030
@@ -9,7 +11,28 @@
 public class BankAccountApp {
     // //// MAIN EXECUTABLE SECTION /////////////////////////////
     public static void main(String[] args) {
-        
+        System.out.println("/////////////////////////////////////////////////////");
+        System.out.println("BANK ACCOUNT APPLICATION\n");
+
+        System.out.println("*****************************************************");
+        System.out.println("Creat 5 Bank Accounts");
+        ArrayList<BankAccount> bankAccountList = new ArrayList<BankAccount>();
+        bankAccountList.add(new BankAccount());
+        bankAccountList.add(new BankAccount());
+        bankAccountList.add(new BankAccount());
+        bankAccountList.add(new BankAccount());
+        bankAccountList.add(new BankAccount());
+
+        printBankAccountList(bankAccountList);
+    }
+    
+    public static void printBankAccountList(ArrayList<BankAccount> bankAccountList) {
+        System.out.println("==== Displaying all bank accounts ========");
+        for (BankAccount bankAccount : bankAccountList) {
+            bankAccount.display();
+        }
+        System.out.println("Total Number of bank accounts: " + BankAccount.getNumTotalBankAccounts());
+        System.out.printf("Total Bank Account Balance: $%.2f\n", BankAccount.getTotalBankAccountBalance());
     }
     
 }
