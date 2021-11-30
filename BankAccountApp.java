889fa27b9cf08c1c9be1cb6b03118bfe55d38a9e
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-11-29 20:40:44 -0800

    initialize

---
+++ BankAccountApp.java
@@ -0,0 +1,15 @@
+//  /////////////////////////////////////////////////////////////
+//  BANK ACCOUNT APP
+//  By: Virgilio D. Cabading Jr.    Created at: November 29, 2021 2030
+//  /////////////////////////////////////////////////////////////
+
+/**
+ * BankAccountApp
+ */
+public class BankAccountApp {
+    // //// MAIN EXECUTABLE SECTION /////////////////////////////
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Bank Account Read Me
+
+This is a program for Coding Dojo > Java Stack > Object Oriented Programming
+
+## In this program, the programmer will demonstrate an ability to:
+1. Use member variable
+2. Implement instance methods and getter and setters
+3. Implement static variables and methods
